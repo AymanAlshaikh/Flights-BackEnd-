b252e342e52e658a63d74801c3d09ece8b8f6b3a
IsmailQamber <ism-you-95@hotmail.com>
2021-03-07 19:39:18 +0300

    User routes done

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const passport = require("passport");
+const Router = express.Router();
+
+const { signup, UserList, signin } = require(""); //DON'T FOR GET TO IMPORT THE CONTROLLERS
+
+//User List router
+Router.get("/", UserList);
+//User sign up
+Router.post("/signup", signup);
+//User sign in
+Router.post(
+  "/signin",
+  passport.authenticate("local", { session: false }),
+  signin
+);
+
+module.exports = Router;
